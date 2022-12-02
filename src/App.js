54dcc89f60fc4a5e7cf28908fc78b4b4e2d900fe
Mikhail <101399547+uristinjava@users.noreply.github.com>
2022-12-02 12:26:49 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+import Message from './components/Message';
+
+function App(props) {
+  return (
+    <div className="App">
+      <header className="App-header">
+
+        <Message />
+
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Message.jsx
@@ -0,0 +1,17 @@
+
+const myName = 'Михаил'
+
+const Message = () => {
+    return (
+        <>
+            <div className="Msg">
+                проба пера в REACT
+                <h3>Привет, {myName}</h3>
+            </div>
+        </>
+    )
+}
+
+
+
+export default Message;
